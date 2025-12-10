37f3edc9635e86cf9c26450517e1737e0db7d8e2
Omar Hamad <etatldr@gmail.com>
2025-12-10 23:47:55 +0200

    Refactor Babel configuration to use @react-native/babel-preset and update package.json dependency

--- telon-gateway-app/babel.config.js
+++ telon-gateway-app/babel.config.js
@@ -1,19 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      'module:metro-react-native-babel-preset',
-      {
-        unstable_transformProfile: 'hermes-stable',
-      },
-    ],
-    [
-      '@babel/preset-typescript',
-      {
-        allowNamespaces: true,
-        allowDeclareFields: true,
-        isTSX: false,
-        onlyRemoveTypeImports: true,
-      },
-    ],
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
